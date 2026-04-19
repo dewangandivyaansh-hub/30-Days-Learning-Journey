18094b89245a092c8d137c9cdb29a3f62fb80bd7
Divyaansh Dewangan <dewangandivyaansh@gmail.com>
2026-04-19 10:26:32 +0530

    Day 23: Added dark mode and dynamic text functionality

---
+++ Day23/script.js
@@ -0,0 +1,9 @@
+function changeText() {
+    document.getElementById("intro").innerText =
+        "Thanks for visiting my portfolio website!";
+}
+
+function toggleTheme() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
